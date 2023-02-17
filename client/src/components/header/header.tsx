cb25ed7c041294507fbfcd3670b861b82af776c8
Ponti-D <spaediaz@gmail.com>
2023-02-17 17:32:04 +0000

    bug fixed nav links, nav items

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -3,7 +3,7 @@ import Nav from "../nav/nav";
  
 const Header: React.FC = () => (
   <header className="header">
-    <h1 className="header__text">Fakelandia Justice Department</h1>
+      <p className="header__text">Fakelandia Justice Department</p>  
     <Nav />
   </header>
 );
--- client/src/components/nav/nav.tsx
+++ client/src/components/nav/nav.tsx
@@ -1,12 +1,35 @@
 import { NavLink } from "react-router-dom";
 
-const Nav : React.FC = () =>
+const Nav: React.FC = () => (
   <nav>
-      <ul className="nav__container">
-          <li className="nav__link"><NavLink to='/home'>Home</NavLink></li>
-          <li className="nav__link"><NavLink to='/misdemeanours'>Misdemeanours</NavLink></li>
-          <li className="nav__link" ><NavLink to='/confession'>Confess To Us</NavLink></li>
-      </ul>
-  </nav>;
+    <ul className="nav__container">
+        
+      <li className="nav__link">
+        <NavLink
+          to="/home"
+          className={({ isActive }) => (isActive ? "link--active" : "link")}
+        >
+          Home
+        </NavLink>
+      </li>
+      <li className="nav__link">
+        <NavLink
+          to="/misdemeanours"
+          className={({ isActive }) => (isActive ? "link--active" : "link")}
+        >
+          Misdemeanours
+        </NavLink>
+      </li>
+      <li className="nav__link">
+        <NavLink
+          to="/confession"
+          className={({ isActive }) => (isActive ? "link--active" : "link")}
+        >
+          Confess To Us
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+);
 
-  export default Nav;
+export default Nav;
