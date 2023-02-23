f927bd4ca791315ef624aef11eb87cbc2908bc08
Ponti-D <spaediaz@gmail.com>
2023-02-23 16:25:54 +0000

    added style to confession form and change font

--- client/src/components/confession/confession.tsx
+++ client/src/components/confession/confession.tsx
@@ -1,20 +1,42 @@
 import React, { useContext, useState } from "react";
-import { MisdemeanoursContext } from "../misdemeanours/misdemeanours";
+import { JustTalk, MisdemeanourKind } from "../../../types/misdemeanours.types";
+import { MisdemeanoursContext } from "../misdemeanours/MisdemeanoursContext";
 
 const Confession: React.FC = () => {
-  const [subject, setSubject] = useState<string>("ww");
-  const [reason, setReason] = useState<string>("just-talk");
-  const [details, setDetails] = useState<string>("wwwwwwwwwwwdddw");
+  const [subject, setSubject] = useState<string>("");
+  const [reason, setReason] = useState<MisdemeanourKind | JustTalk>(
+    "just-talk"
+  );
+  const [details, setDetails] = useState<string>("");
   const [formValid, setFormValid] = useState(false);
   const [validationMessage, setvalidationMessage] = useState("");
   const [hasError, sethasError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setsuccessMessage] = useState("");
 
+  
   const misdemeanour = useContext(MisdemeanoursContext);
+ 
+
+  const onChangeSubject = (e: any) => {
+    e.preventDefault();
+    setsuccessMessage("");
+    setSubject(e.target.value);
+    if (e.target.value.length < 3 || e.target.value.length > 20) {
+      setvalidationMessage(
+        "The detials needs to be between 3 and 20 haractors long."
+      );
+      setFormValid(false); //set submit button status
+    } else {
+     
+      setvalidationMessage("");
+      setFormValid(true);
+    }
+  };
 
   const onChangeDetails = (e: any) => {
     e.preventDefault();
-
+    setsuccessMessage("");
     setDetails(e.target.value);
 
     if (e.target.value.length < 10 || e.target.value.length > 100) {
@@ -30,15 +52,16 @@ const Confession: React.FC = () => {
 
   const onFormSubmit = async (e: any) => {
     e.preventDefault();
-
+    
     const newMisdemeanour = {
       subject,
       reason,
       details,
     };
+
     console.log(newMisdemeanour);
     try {
-      const response = await fetch("http://localhost:8080/api/confesss/", {
+      const response = await fetch("http://localhost:8080/api/confess/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newMisdemeanour),
@@ -46,24 +69,29 @@ const Confession: React.FC = () => {
 
       if (!response.ok) {
         sethasError(true);
-        setErrorMessage(
-          "Error - confession wasn't saved - try again later!"
-        );
+        setErrorMessage("Error - confession wasn't saved - try again later!");
         return;
       }
-      
+
       const data = await response.json();
 
-      console.log(response.status + data.message);
-
+      if (data.message && !data.justTalked) {
+        misdemeanour.push({
+          citizenId: Math.floor(2 + Math.random() * 100),
+          misdemeanour: reason,
+          date: new Date().toLocaleDateString(),
+        });
+      }
+      setsuccessMessage( "Confession submitted " + data.message) ;
+      console.log(response.status + "Confession submitted " + data.message);
     } catch (error) {
       console.log(error);
     }
-
-    // setDetails("");
-    // setSubject("");
-    // setReason("talk");
-    // setIsFormValid(false);
+   
+    setDetails("");
+    setSubject("");
+    setReason("just-talk");
+     setFormValid(false);
   };
 
   return (
@@ -80,26 +108,26 @@ const Confession: React.FC = () => {
         </p>
       </div>
       <div className="form__container">
-        <form onSubmit={onFormSubmit}>
-          <div>
+        <form className="form" onSubmit={onFormSubmit}>
+          <div className="form__controlgroup">
             <label>Subject: </label>
             <input
+            className="form__input"
               type="text"
               required
               value={subject}
-              onChange={(event) => setSubject(event.target.value)}
+              onChange={onChangeSubject}
             />
           </div>
-          <div>
-            <label> Reason for contact :</label>
+          <div className="form__controlgroup">
+            <label> Reason for contact:</label>
             <select
               defaultValue="just-talk"
-              className="reason"
+              className="form__input"
               name="reason"
               required
-              onChange={(e) => setReason(e.target.value)}
+              onChange={(e) => setReason(e.target.value as MisdemeanourKind)}
             >
-              <option value="">Reason for confess</option>
               <option value="rudeness">Mild Public Rudeness 🤪</option>
               <option value="vegetables">Not Eating Your Vegetables 🥗 </option>
               <option value="lift">Speaking in a Lift 🗣</option>
@@ -107,28 +135,38 @@ const Confession: React.FC = () => {
               <option value="just-talk">I just want to talk 😬</option>
             </select>
           </div>
-          <div>
-            <label>Details here ....</label>
+          <div className="form__controlgroup form__controlgroup--details">
+            <label htmlFor="details">Details here 👉</label>
             <textarea
               name="details"
-              className="confess_details"
+              className="form__input--details"  
               value={details}
               rows={5}
               onChange={onChangeDetails}
               required
+              
             />
           </div>
-          {!formValid && (
+          {!formValid && validationMessage.length > 0 && (
             <div>
-              <label>{validationMessage}</label>
+              <label htmlFor="validationMessage">
+                Please correct your information
+              </label>
+              <p>{validationMessage}</p>
             </div>
           )}
-          { hasError && (
+          {hasError && (
             <div>
-              <label>{errorMessage}</label>
+              <label htmlFor="errorMessage">Errors</label>
+              <p>{errorMessage}</p>
             </div>
           )}
-          <button disabled={!formValid}> Submit 🤓</button>
+          <div className="submit__container">
+            <button disabled={!formValid || hasError}> Submit 🤓</button>
+            {successMessage.length > 0 && (
+              <p className="success__message">{successMessage}</p>
+            )}
+          </div>
         </form>
       </div>
     </>
