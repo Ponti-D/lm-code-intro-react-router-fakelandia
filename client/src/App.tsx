ed13e83fe6f761c8e5d81be681de54f53f7ad67c
Ponti-D <spaediaz@gmail.com>
2023-02-25 22:25:37 +0000

    Following feedback - mode fetch call inside useEffect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,23 +8,23 @@ import {
 import { Router } from "./components/router/router";
 
 function App() {
+   
   const [misdemeanours, setMisdemeanours] = useState<MisdemeanourProps[]>([]);
   const amount = 10;
 
-  const getMisdemeanours = async (amount: number) => {
-    const apiResponse = await fetch(
-      `http://localhost:8080/api/misdemeanours/${amount}`
-    );
-    const json = await apiResponse.json();
-    setMisdemeanours(json.misdemeanours);
-  };
-
   useEffect(() => {
+    const getMisdemeanours = async (amount: number) => {
+    try {
+      const apiResponse = await fetch(
+        `http://localhost:8080/api/misdemeanours/${amount}`
+      );
+      const json = await apiResponse.json();
+      setMisdemeanours(json.misdemeanours);
+    } catch (error) {}
+  };
     getMisdemeanours(amount);
-    console.log("use effect");
   }, []);
-
-  useContext(MisdemeanoursContext);
+  
   return (
     <MisdemeanoursContext.Provider value={misdemeanours}>
       <BrowserRouter>
