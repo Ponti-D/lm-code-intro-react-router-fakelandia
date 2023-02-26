321274cf018adaab275c494b8e44c855f8f31faf
Ponti-D <spaediaz@gmail.com>
2023-02-26 18:56:55 +0000

    added tests to confession form

--- client/src/components/confession/confession.test.tsx
+++ client/src/components/confession/confession.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Confession from "./confession";
 
 describe("Confession form", () => {
@@ -10,6 +11,26 @@ describe("Confession form", () => {
     expect(screen.getByText(/Details here/i)).toBeInTheDocument();
 
   });
+
+  test("submit enabled", async () => {
   
+    render(<Confession />);
+     await userEvent.type(
+      screen.getByRole("textbox", {name: /subject/i}),
+      "This is subject"
+    );
+    await userEvent.type(
+      screen.getByRole("textbox", {name: /details/i}),
+      "This is details xx xnnxnx nxnnxnx"
+    );
+
+ 
+   expect(screen.getByRole('button', { name: /submit/i, })).toBeEnabled();
+ 
+   
+  });
+
+
+
 });
 
